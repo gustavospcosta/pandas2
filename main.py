f5a663ae96bfbb8257733f3aec24328463980fec
gustavospcosta <93624837+gustavospcosta@users.noreply.github.com>
2023-07-28 15:13:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,115 +1,115 @@
-# ---------------- MODULOS PYTHON ---------------- #
-#IMPORTANDO modulos
+# ---------------- PYTHON MODULES ---------------- #
+#IMPORTING modules
 from datetime import datetime
 import pandas as pd
 from lxml import etree
 import re
-# ---------------- USO GERAL ---------------- #
-#ARQUIVO HTML
+# ---------------- GENERAL USE ---------------- #
+#HTML FILE
 arq_html = r"C:/pbix/DPA ADMINISTRATIVO.htm"
-#ARQUIVO MEDIDAS
+#MEASURES FILE
 arq_MEDIDAS = r"C:/files/xlsx/Base_MEDIDAS.xlsx"
-#ARQUIVO TABELAS
+#TABLES FILE
 arq_TABELAS = r"C:/files/xlsx/Base_TABELAS.xlsx"
-#ARQUIVO BANCOS
+#BANKS FILE
 arq_BANCOS = r"C:/files/xlsx/Base_BANCOS.xlsx"
-#DIRETORIO BKP
+#BACKUP DIRECTORY
 dir_bkp = r"C:/files/bkp_xlsx"
-#Lendo arquivo HTML usando Pandas
+#Reading HTML file using Pandas
 doc = pd.read_html(arq_html)
-#LENDO ARQUIVO XLSX MEDIDAS
+#READING XLSX FILE MEASURES
 plan_MEDIDAS = pd.read_excel(arq_MEDIDAS)
-#LENDO ARQUIVO XLSX TABELAS
+#READING XLSX FILE TABLES
 plan_TABELAS = pd.read_excel(arq_TABELAS)
-#LENDO ARQUIVO XLSX BANCOS
+#READING XLSX FILE BANKS
 plan_BANCOS = pd.read_excel(arq_BANCOS)
-#DATAFRAME VAZIO
-df_vazio = pd.DataFrame(columns = ['NOME_DO_BANCO','TABELA_BANCO','CONSULTA_SQL'])
-#VARIAVEL DATA E HORA ATUAL
-agora = datetime.now()
-data_hora = agora.strftime("%d/%m/%Y %H:%M:%S")
-#Lê o arquivo HTML formata com parse e salva na variavel e Pega os valores das TAGs do HTML e armazena numa lista
+#EMPTY DATAFRAME
+df_vazio = pd.DataFrame(columns = ['BANK_NAME','BANK_TABLE','SQL_QUERY'])
+#VARIABLE CURRENT DATE AND TIME
+now = datetime.now()
+date_time = now.strftime("%d/%m/%Y %H:%M:%S")
+#Read the HTML file format with parse and save in the variable and Get the values of the HTML TAGs and store in a list
 doc_html = etree.parse(arq_html, etree.HTMLParser(encoding='utf-8'))
-nome_rel_arq = doc_html.xpath('/html/body/h2[2]/div/text()')[0]
-nome_rel_arq = nome_rel_arq.replace('File:','')
-nome_rel_arq = nome_rel_arq.replace('.pbix','')
-data_rel_arq = doc_html.xpath('/html/body/h2[1]/div[2]/text()')
-#Passando a TABELA DE MEDIDAS do HTML que queremos utilizar para a variavel
+report_name_file = doc_html.xpath('/html/body/h2[2]/div/text()')[0]
+report_name_file = report_name_file.replace('File:','')
+report_name_file = report_name_file.replace('.pbix','')
+report_date_file = doc_html.xpath('/html/body/h2[1]/div[2]/text()')
+#Passing the MEASURES TABLE from HTML that we want to use to the variable
 tb_medidas = doc[6]
-#Passando a TABELA TABELAS do HTML que queremos utilizar para a variavel
+#Passing the TABLES TABLE from HTML that we want to use to the variable
 tb_tabelas = doc[11]
-#Passando a TABELA BANCOS do HTML que queremos utilizar para a variavel
+#Passing the BANKS TABLE from HTML that we want to use to the variable
 tb_bancos = doc[10]
-# ---------------- BASE_MEDIDAS ---------------- #
-#Criando DataFrame da TABELA com UNICA coluna
+# ---------------- BASE_MEASURES ---------------- #
+#Creating DataFrame from TABLE with SINGLE column
 df_temp = tb_medidas[1]
-#RETIRANDO VALORES DUPLICADOS do DATAFRAME de UNICA COLUNA
-df_filtrado = df_temp.drop_duplicates()
-#PASSANDO VALOR DOS INDICES CONTIDOS NO INTERVALO do DATAFRAME de UNICA COLUNA
-df_filtrado = df_filtrado[1:10]
-#CRIANDO a LISTA
-lista00 = df_filtrado.values.tolist()
-#CRIANDO DATAFRAME A PARTIR DA LISTA
-df_origem_m = pd.DataFrame(lista00, columns=['Consulta_PowerBI'])
-#ADICIONANDO Colunas e valores para o DataFrame
-df_origem_m['Nome_Relatorio'] = nome_rel_arq
-df_origem_m['Data_REF_ARQ'] = data_rel_arq[0]
-df_origem_m['Data_REF'] = data_hora
-#Criando DataFrame
-df_origem_m = df_origem_m[['Nome_Relatorio','Consulta_PowerBI','Data_REF_ARQ','Data_REF']]
-#FILTRANDO o DataFrame pelo Nome do Relatorio
-plan_MEDIDAS = plan_MEDIDAS[plan_MEDIDAS.Nome_Relatorio != nome_rel_arq]
-#CONCATENANDO DATAFRAMES
-dt_destino_m = pd.concat([plan_MEDIDAS, df_origem_m],ignore_index=True,sort=False)
-#Salvando o DataFrame na planilha usando Pandas
-dt_destino_m.to_excel(arq_MEDIDAS,index=False)
-# ---------------- BASE_TABELAS ---------------- #
-#ADICIONANDO colunas e VALORES no Dataframe TABELAS
-tb_tabelas['Nome_Relatorio'] = nome_rel_arq
-tb_tabelas['Data_REF_ARQ'] = data_rel_arq[0]
-tb_tabelas['Data_REF'] = data_hora
-#Criando DataFrame da TABELA DE MEDIDAS com colunas especificas
-df_origem_t = tb_tabelas[["Nome_Relatorio","Measure Name","Expression","Data_REF_ARQ","Data_REF"]]
-#FILTRANDO o DataFrame pelo Nome do Relatorio
-plan_TABELAS = plan_TABELAS[plan_TABELAS.Nome_Relatorio != nome_rel_arq]
-#CONCATENANDO DATAFRAMES
-df_destino_t = pd.concat([plan_TABELAS, df_origem_t],ignore_index=True,sort=False)
-#Salvando o DataFrame na planilha usando Pandas
-df_destino_t.to_excel(arq_TABELAS, index=False)
-# ---------------- BASE_BANCOS ---------------- #
-#PARAMETROS para busca e limpeza da string
+#REMOVING DUPLICATE VALUES from SINGLE COLUMN DATAFRAME
+df_filtered = df_temp.drop_duplicates()
+#PASSING VALUE OF INDICES CONTAINED IN THE RANGE from SINGLE COLUMN DATAFRAME
+df_filtered = df_filtered[1:10]
+#CREATING the LIST
+list00 = df_filtered.values.tolist()
+#CREATING DATAFRAME FROM THE LIST
+df_source_m = pd.DataFrame(list00, columns=['PowerBI_Query'])
+#ADDING Columns and values for the DataFrame
+df_source_m['Report_Name'] = report_name_file
+df_source_m['File_REF_DATE'] = report_date_file[0]
+df_source_m['REF_DATE'] = date_time
+#Creating DataFrame
+df_source_m = df_source_m[['Report_Name','PowerBI_Query','File_REF_DATE','REF_DATE']]
+#FILTERING the DataFrame by Report Name
+plan_MEDIDAS = plan_MEDIDAS[plan_MEDIDAS.Report_Name != report_name_file]
+#CONCATENATING DATAFRAMES
+dt_dest_m = pd.concat([plan_MEDIDAS, df_source_m],ignore_index=True,sort=False)
+#Saving the DataFrame in the spreadsheet using Pandas
+dt_dest_m.to_excel(arq_MEDIDAS,index=False)
+# ---------------- BASE_TABLES ---------------- #
+#ADDING columns and VALUES in the TABLES Dataframe
+tb_tabelas['Report_Name'] = report_name_file
+tb_tabelas['File_REF_DATE'] = report_date_file[0]
+tb_tabelas['REF_DATE'] = date_time
+#Creating DataFrame of MEASURES TABLE with specific columns
+df_source_t = tb_tabelas[["Report_Name","Measure Name","Expression","File_REF_DATE","REF_DATE"]]
+#FILTERING the DataFrame by Report Name
+plan_TABELAS = plan_TABELAS[plan_TABELAS.Report_Name != report_name_file]
+#CONCATENATING DATAFRAMES
+df_dest_t = pd.concat([plan_TABELAS, df_source_t],ignore_index=True,sort=False)
+#Saving the DataFrame in the spreadsheet using Pandas
+df_dest_t.to_excel(arq_TABELAS, index=False)
+# ---------------- BASE_BANKS ---------------- #
+#PARAMETERS for search and string cleaning
 a = "#(lf)"
 a = re.escape(a)
 b = r"FROM (.+?) "
 j = "JOIN (.+?) "
-aspas = '"'
-lista = []
-re_banco = 'e.Database("'
-re_banco = r''+re.escape(re_banco)+'(.+?)'+aspas+''
-fim_query = ')]'
-fim_query = re.escape(fim_query)
-re_query = r'Query=(.+?)'+aspas+''
-df_origem_b = pd.DataFrame(columns=["Banco","Query"])
-#CRIANDO LISTA
-lista = tb_bancos.iloc[:,3]
-#INICIANDO LOOP PARA LIMPAR STRING CONTIDA EM CADA POSICAO DA LISTA
-for linha in lista:
-  #Limpa caracter #(lf)
-  linha = re.sub(r"" + a + "", "", str(linha))
-  banco = re.findall(re_banco, linha, re.DOTALL)
-  query = re.findall(re_query, linha, re.DOTALL)
-  df = {"Banco":banco, "Query":query}
+quotes = '"'
+list = []
+re_bank = 'e.Database("'
+re_bank = r''+re.escape(re_bank)+'(.+?)'+quotes+''
+end_query = ')]'
+end_query = re.escape(end_query)
+re_query = r'Query=(.+?)'+quotes+''
+df_source_b = pd.DataFrame(columns=["Bank","Query"])
+#CREATING LIST
+list = tb_bancos.iloc[:,3]
+#INITIATING LOOP TO CLEAN STRING CONTAINED IN EACH POSITION OF THE LIST
+for line in list:
+  #Cleans character #(lf)
+  line = re.sub(r"" + a + "", "", str(line))
+  bank = re.findall(re_bank, line, re.DOTALL)
+  query = re.findall(re_query, line, re.DOTALL)
+  df = {"Bank":bank, "Query":query}
   df = pd.DataFrame(df)
-  df_origem_b = df_origem_b.append(df, ignore_index = True)
-#ADICIONANDO colunas e VALORES no Dataframe BANCOS
-df_origem_b['Nome_Relatorio'] = nome_rel_arq
-df_origem_b['Data_REF_ARQ'] = data_rel_arq[0]
-df_origem_b['Data_REF'] = data_hora
-#Criando DataFrame da TABELA DE BANCOS com colunas especificas
-df_origem_b = df_origem_b
-#FILTRANDO o DataFrame pelo Nome do Relatorio
-plan_BANCOS = plan_BANCOS[plan_BANCOS.Nome_Relatorio != nome_rel_arq]
-#CONCATENANDO DATAFRAMES
-df_destino_b = pd.concat([plan_BANCOS, df_origem_b],ignore_index=True,sort=False)
-#Salvando o DataFrame na planilha usando Pandas
-df_destino_b.to_excel(arq_BANCOS, index=False)
+  df_source_b = df_source_b.append(df, ignore_index = True)
+#ADDING columns and VALUES in the BANKS Dataframe
+df_source_b['Report_Name'] = report_name_file
+df_source_b['File_REF_DATE'] = report_date_file[0]
+df_source_b['REF_DATE'] = date_time
+#Creating DataFrame of BANKS TABLE with specific columns
+df_source_b = df_source_b
+#FILTERING the DataFrame by Report Name
+plan_BANCOS = plan_BANCOS[plan_BANCOS.Report_Name != report_name_file]
+#CONCATENATING DATAFRAMES
+df_dest_b = pd.concat([plan_BANCOS, df_source_b],ignore_index=True,sort=False)
+#Saving the DataFrame in the spreadsheet using Pandas
+df_dest_b.to_excel(arq_BANCOS, index=False)
